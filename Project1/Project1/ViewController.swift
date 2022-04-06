da7555d6fbd24b4602ec9fbde3e369db5f4dc522
DingDing0919 <dingding19940919@icloud.com>
2022-04-06 21:39:28 +0800

    upgrade

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // build Label
+    var myLabel:UILabel? = nil
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
