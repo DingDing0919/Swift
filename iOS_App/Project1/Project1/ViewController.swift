c4467e502a5fe67ba66aba27fcb84822f437a7e1
Chien, Cheng Wei <dingding19940919@icloud.com>
2022-04-06 15:17:27 +0800

    new project upgrade

---
+++ iOS_App/Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by 簡鉦衛 on 2022/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
