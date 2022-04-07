efa0c23224de0e898aee38132e2abef711241f7a
DingDing0919 <dingding19940919@icloud.com>
2022-04-07 18:38:04 +0800

    modify

--- Lesson 4/Lesson4.playground/Contents.swift
+++ Lesson 4/Lesson4.playground/Contents.swift
@@ -6,3 +6,16 @@ someInts.append(3)
 someInts.append(5)
 someInts.append(7)
 
+// 建立陣列, Array init
+var threeDoubles = Array(repeating: 0.0, count: 3)
+print("threeDoubles:\(threeDoubles)")
+
+var anotherThreeDobles = Array(repeating: 2.9, count: 4)
+print("anotherThreeDoubles:\(anotherThreeDobles)")
+
+// Add
+print("threeDoubles + anotherThreeDobles:\(threeDoubles + anotherThreeDobles)")
+
+// Array
+var newArray = ["Ding", "Pig", "Dog"]
+print("newArray:\(newArray)")
